586efac95d1898bb0d08677db6386638a70a3a14
Valentinmagde <38159415+Valentinmagde@users.noreply.github.com>
2023-11-23 22:53:16 +0400

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -19,17 +19,30 @@ set('rsync_src', function () {
 // add('shared_dirs', []);
 // add('writable_dirs', []);
 
-add('rsync', [
-    'exclude' => [
-        '.git',
-        '/.env',
-        '/storage/',
-        '/vendor/',
-        '/node_modules/',
-        '.github',
-        'deploy.php',
-    ],
-]);
+// add('rsync', [
+//     'exclude' => [
+//         '.git',
+//         '/.env',
+//         '/storage/',
+//         '/vendor/',
+//         '/node_modules/',
+//         '.github',
+//         'deploy.php',
+//     ],
+// ]);
+
+host('production')
+    ->set('hostname', '137.184.133.101')
+    ->set('remote_user', 'root')
+    ->set('labels', ['stage' => 'production'])
+    ->set('deploy_path', '/var/www/lumen-boiler-template');
+
+host('staging')
+    ->set('hostname', '137.184.133.101')
+    ->set('remote_user', 'root')
+    ->set('port', '22')
+    ->set('labels', ['stage' => 'staging'])
+    ->set('deploy_path', '/var/www/api-beachcomber-v3');
 
 set('lock_path', '{{deploy_path}}/deploy.lock');
 
@@ -52,61 +65,37 @@ task('deploy:unlock', function () {
     }
 });
 
-after('deploy:prepare', 'deploy:lock');
-after('deploy:symlink', 'deploy:unlock');
-
 task('deploy:secrets', function () {
     file_put_contents(__DIR__ . '/.env', getenv('DOT_ENV'));
     upload('.env', get('deploy_path') . '/shared');
 });
 
-host('production')
-    ->set('hostname', '137.184.133.101')
-    ->set('remote_user', 'root')
-    ->set('labels', ['stage' => 'production'])
-    ->set('deploy_path', '/var/www/lumen-boiler-template');
+task('deploy:prepare', [
+    'deploy:info',
+    'deploy:setup',
+    'deploy:lock',
+    'deploy:release',
+    'deploy:update_code',
+    'deploy:shared',
+    'deploy:writable',
+])->desc('Prepares a new release');
 
-host('staging')
-    ->set('hostname', '137.184.133.101')
-    ->set('remote_user', 'root')
-    ->set('port', '22')
-    ->set('labels', ['stage' => 'staging'])
-    ->set('deploy_path', '/var/www/api-beachcomber-v3');
-
-after('deploy:failed', 'deploy:unlock');
-
-// task('deploy:prepare', [
-//     'deploy:info',
-//     'deploy:setup',
-//     'deploy:lock',
-//     'deploy:release',
-//     'deploy:update_code',
-//     'deploy:shared',
-//     'deploy:writable',
-// ])->desc('Prepares a new release');
-
-// task('deploy:publish', [
-//     'deploy:secrets',
-//     'deploy:vendors',
-//     'artisan:migrate',
-//     'deploy:symlink',
-//     'deploy:unlock',
-//     'deploy:cleanup',
-//     'deploy:success',
-     
-// ])->desc('Publishes the release');
-
-// task('deploy', [
-//     'deploy:prepare',
-//     'deploy:publish'
-// ])->desc('Deploy the application');
-/**
- * Main deploy task.
- */
-desc('Deploys your project');
-task('deploy', [
-    'deploy:prepare',
+task('deploy:publish', [
+    'deploy:secrets',
     'deploy:vendors',
     'artisan:migrate',
-    'deploy:publish',
-]);
+    'deploy:symlink',
+    'deploy:unlock',
+    'deploy:cleanup',
+    'deploy:success',
+     
+])->desc('Publishes the release');
+
+task('deploy', [
+    'deploy:prepare',
+    'deploy:publish'
+])->desc('Deploy the application');
+
+after('deploy:prepare', 'deploy:lock');
+after('deploy:symlink', 'deploy:unlock');
+after('deploy:failed', 'deploy:unlock');
