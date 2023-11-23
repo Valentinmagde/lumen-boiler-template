bde811e9da251d757eb20ad9147cda152a628194
Valentinmagde <valentinmagde@gmail.com>
2023-11-23 11:18:11 +0400

    update deploy file

--- deploy.php
+++ deploy.php
@@ -4,6 +4,7 @@ namespace Deployer;
 
 require 'recipe/laravel.php';
 require 'contrib/rsync.php';
+use Deployer\Exception\GracefulShutdownException;
 
 set('repository', 'https://github.com/Valentinmagde/lumen-boiler-template.git');
 set('application', 'API V2');
@@ -84,6 +85,35 @@ host('staging')
 //     'deploy:cleanup',
 //     'deploy:success',
 // ]);
+
+desc('Locks deploy');
+task('deploy:lock', function () {
+    $user = escapeshellarg(get('user'));
+    $locked = run("[ -f {{deploy_path}}/.dep/deploy.lock ] && echo +locked ||
+    echo $user > {{deploy_path}}/.dep/deploy.lock");
+    if ($locked === '+locked') {
+        $lockedUser = run("cat {{deploy_path}}/.dep/deploy.lock");
+        throw new GracefulShutdownException(
+            "Deploy locked by $lockedUser.\n" .
+            "Execute \"deploy:unlock\" task to unlock."
+        );
+    }
+});
+
+desc('Unlocks deploy');
+task('deploy:unlock', function () {
+    run("rm -f {{deploy_path}}/.dep/deploy.lock");//always success
+});
+
+desc('Checks if deploy is locked');
+task('deploy:is_locked', function () {
+    $locked = test("[ -f {{deploy_path}}/.dep/deploy.lock ]");
+    if ($locked) {
+        $lockedUser = run("cat {{deploy_path}}/.dep/deploy.lock");
+        throw new GracefulShutdownException("Deploy is locked by $lockedUser.");
+    }
+    info('Deploy is unlocked.');
+});
 
 desc('Deploys your project');
 task('deploy', [
